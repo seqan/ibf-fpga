5d1c7196e66a8df9f495f5173d965bff6cd66df0
Tobias Baumann <tobias.baumann@zib.de>
2023-03-08 21:27:35 +0100

    [Device] Move asserts, static helpers, types and conversions to header

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -6,61 +6,6 @@
 #include "kernel.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Asserts
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-static_assert(WINDOW_SIZE >= K, "Window size needs to be greater or equal K-mer size");
-
-static_assert(sizeof(seeds) / sizeof(HostHash) >= HASH_COUNT, "The number of hash functions must be smaller or equal the number of seeds");
-
-static_assert(sizeof(HostHash) >= sizeof(MINIMIZER_SEED), "Minimizer seed doesn't fit Hash type");
-static_assert(sizeof(HostHash) * 8 >= 2 * K, "K-mer doesn't fit Hash type");
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Static Helper
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-template<class LHS, class RHS>
-constexpr auto INTEGER_DIVISION_CEIL(const LHS lhs, const RHS rhs)
-{
-	return (lhs + rhs - 1) / rhs;
-}
-
-constexpr auto NUMBER_OF_KMERS_PER_WINDOW = WINDOW_SIZE - K + 1;
-
-constexpr auto INITIALIZATION_ITERATIONS = (K - 1) + (NUMBER_OF_KMERS_PER_WINDOW - 1);
-
-constexpr auto MINIMIZER_SEED_ADJUSTED = MINIMIZER_SEED >> (64 - 2 * K);
-
-constexpr auto TECHNICAL_BIN_COUNT_WORDS = INTEGER_DIVISION_CEIL(BIN_COUNT, HOST_SIZE_TYPE_BITS);
-constexpr auto TECHNICAL_BIN_COUNT = TECHNICAL_BIN_COUNT_WORDS * HOST_SIZE_TYPE_BITS;
-
-constexpr auto CHUNK_BITS = TECHNICAL_BIN_COUNT > MAX_BUS_WIDTH? MAX_BUS_WIDTH : TECHNICAL_BIN_COUNT;
-constexpr auto CHUNKS = TECHNICAL_BIN_COUNT > MAX_BUS_WIDTH? INTEGER_DIVISION_CEIL(TECHNICAL_BIN_COUNT, MAX_BUS_WIDTH) : 1;
-constexpr auto CHUNKS_PER_BIN = INTEGER_DIVISION_CEIL(TECHNICAL_BIN_COUNT, MAX_BUS_WIDTH);
-
-static_assert((bool)(TECHNICAL_BIN_COUNT < MAX_BUS_WIDTH) || (TECHNICAL_BIN_COUNT % MAX_BUS_WIDTH == 0), "Bin count needs to be less or a multiple of max bus width");
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// Types & Conversions
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-using HostSizeType = ac_int<HOST_SIZE_TYPE_BITS, false>;
-
-using Element = ac_int<2, false>;
-using Hash = ac_int<2 * K, false>;
-
-using QueryIndex = ac_int<25, true>;
-
-using Minimizer = struct
-{
-	Hash hash;
-	unsigned char position;
-};
-
-using Chunk = ac_int<CHUNK_BITS, false>;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 // Minimizer
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
