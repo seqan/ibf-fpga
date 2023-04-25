92f8f7c0e2f34ab65158dfaaff648eb5d005950a
Tobias Baumann <tobias.baumann@zib.de>
2023-04-25 15:58:34 +0200

    Add ibf_sycl_test

--- include/min_ibf_fpga/backend_sycl/kernel_ibf.hpp
+++ include/min_ibf_fpga/backend_sycl/kernel_ibf.hpp
@@ -1,8 +1,13 @@
 #include <sycl/sycl.hpp>
-#include <sycl/ext/intel/fpga_extensions.hpp>
 #include <sycl/ext/intel/ac_types/ac_int.hpp>
+#include <sycl/ext/intel/fpga_extensions.hpp>
 
 #include "commen.hpp"
+#include "pipe_utils.hpp"
+#include "unrolled_loop.hpp"
+
+namespace min_ibf_fpga::backend_sycl
+{
 
 inline HostSizeType mapTo(const HostHash hash, const HostSizeType binSize)
 {
@@ -40,6 +45,7 @@ inline Counter getThreshold(const HostSizeType numberOfHashes,
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Forward declaration of the kernel names. FPGA best practice to reduce compiler name mangling in the optimization reports.
+class HostToKernelPipe;
 class IBFkernel;
 
 void RunIBFKernel(sycl::queue& queue,
@@ -53,83 +59,122 @@ void RunIBFKernel(sycl::queue& queue,
 	sycl::buffer<HostSizeType, 1>& thresholds_buffer,
 	sycl::buffer<Chunk, 1>& result_buffer)
 {
-	queue.submit([&](sycl::handler &handler)
+	using MinimizerToIBFPipes = fpga_tools::PipeArray<class MinimizerToIBFPipe, MinimizerToIBFData, 25, NUMBER_OF_KERNELS>;
+
+	fpga_tools::UnrolledLoop<NUMBER_OF_KERNELS>([&](auto id)
 	{
-		sycl::accessor minimizerToIbf(minimizerToIbf_buffer, handler, sycl::read_only);
-		sycl::accessor ibfData(ibfData_buffer, handler, sycl::read_only);
-		sycl::accessor thresholds(thresholds_buffer, handler, sycl::read_only);
-		sycl::accessor result(result_buffer, handler, sycl::write_only);
-		handler.single_task<IBFkernel>([=]() [[intel::kernel_args_restrict]]
+		queue.submit([&](sycl::handler &handler)
 		{
-			for (QueryIndex queryIndex = 0; queryIndex < (QueryIndex)numberOfQueries; queryIndex++)
+			sycl::accessor minimizerToIbf(minimizerToIbf_buffer, handler, sycl::read_only);
+
+			handler.single_task<HostToKernelPipe>([=]() [[intel::kernel_args_restrict]]
 			{
-				#define UNSAFELEN 9 // LD singlepump pump (7) + Arithmetic (1) + Store (1)
-				#define II (UNSAFELEN - CHUNKS)
-				#if II > 1
-					Counter counters[CHUNKS][CHUNK_BITS]; // __attribute__((register)) 
-				#else
-					Counter __attribute__((
-							memory,
-							numbanks(1),
-							bankwidth(CHUNK_BITS * sizeof(Counter)),
-							singlepump,//singlepump,
-							max_replicates(1)))
-						counters[CHUNKS][CHUNK_BITS];
-				#endif
-				bool countersInitialized = 0;
-				QueryIndex numberOfHashes = 1;
-				MinimizerToIBFData data;
-				#if II <= 1
-					#pragma ivdep array(counters)
-				#endif
-				do
+				for (size_t i = 0; i < minimizerToIbf.size(); i++)
 				{
-					data = minimizerToIbf[static_cast<size_t>(numberOfHashes-1)];
-					const QueryIndex localNumberOfHashes = numberOfHashes++;
-					Counter threshold;
-					if (data.isLastElement) {
-							//threshold = getThreshold(localNumberOfHashes, minimalNumberOfMinimizers, maximalNumberOfMinimizers, thresholds);
-							// manual inline of getThreshold() because of thresholds accessor
-							const HostSizeType maximalIndex = maximalNumberOfMinimizers - minimalNumberOfMinimizers;
-							HostSizeType index = localNumberOfHashes < minimalNumberOfMinimizers? 0 : localNumberOfHashes - minimalNumberOfMinimizers;
-							index = index < maximalIndex? index : maximalIndex;
-							threshold = (thresholds[static_cast<size_t>(index)] + 2).to_uint();
-					}
-					HostSizeType binOffsets[HASH_COUNT];
-					#pragma unroll
-					for (unsigned char seedIndex = 0; seedIndex < HASH_COUNT; ++seedIndex)
-						binOffsets[seedIndex] = calculateBinIndex(data.hash,
-							seedIndex, hashShift, binSize) * CHUNKS_PER_BIN;
-					for (unsigned char chunkIndex = 0; chunkIndex < CHUNKS; chunkIndex++)
+					MinimizerToIBFPipes::PipeAt<id>::write(minimizerToIbf[i]);
+				}
+			});
+		});
+
+		queue.submit([&](sycl::handler &handler)
+		{
+			sycl::accessor ibfData(ibfData_buffer, handler, sycl::read_only);
+			sycl::accessor thresholds(thresholds_buffer, handler, sycl::read_only);
+			sycl::accessor result(result_buffer, handler, sycl::write_only);
+
+			handler.single_task<IBFkernel>([=]() [[intel::kernel_args_restrict]]
+			{
+				for (QueryIndex queryIndex = 0; queryIndex < (QueryIndex)numberOfQueries; queryIndex++)
+				{
+					#define UNSAFELEN 9 // LD singlepump pump (7) + Arithmetic (1) + Store (1)
+					#define II (UNSAFELEN - CHUNKS)
+
+					#if II > 1
+						Counter counters[CHUNKS][CHUNK_BITS]; // __attribute__((register))
+					#else
+						Counter __attribute__((
+								memory,
+								numbanks(1),
+								bankwidth(CHUNK_BITS * sizeof(Counter)),
+								singlepump,//singlepump,
+								max_replicates(1)))
+							counters[CHUNKS][CHUNK_BITS];
+					#endif
+					bool countersInitialized = 0;
+
+					QueryIndex numberOfHashes = 1;
+
+					MinimizerToIBFData data;
+
+					#if II <= 1
+						#pragma ivdep array(counters)
+					#endif
+					do
 					{
-						Chunk bitvector = ~(Chunk)0;
-						Chunk localResult = 0;
-						// Unroll: Burst-coalesced over chunks per seed
+						data = MinimizerToIBFPipes::PipeAt<id>::read();
+
+						const QueryIndex localNumberOfHashes = numberOfHashes++;
+
+						Counter threshold;
+
+						if (data.isLastElement) {
+								//threshold = getThreshold(localNumberOfHashes, minimalNumberOfMinimizers, maximalNumberOfMinimizers, thresholds);
+								// manual inline of getThreshold() because of thresholds accessor
+								const HostSizeType maximalIndex = maximalNumberOfMinimizers - minimalNumberOfMinimizers;
+
+								HostSizeType index = localNumberOfHashes < minimalNumberOfMinimizers? 0 : localNumberOfHashes - minimalNumberOfMinimizers;
+								index = index < maximalIndex? index : maximalIndex;
+
+								threshold = (thresholds[static_cast<size_t>(index)] + 2).to_uint();
+						}
+
+						HostSizeType binOffsets[HASH_COUNT];
+
 						#pragma unroll
 						for (unsigned char seedIndex = 0; seedIndex < HASH_COUNT; ++seedIndex)
-							bitvector &= //__burst_coalesced_cached_load(
-								/*&*/ibfData[static_cast<size_t>(binOffsets[seedIndex]) + chunkIndex];//,
-								//1048576); // 1 MiB = 8 megabit
-								//65536); // 65536 byte = 512 kilobit (default)
-						#pragma unroll
-						for (ushort bitOffset = 0; bitOffset < CHUNK_BITS; ++bitOffset)
+							binOffsets[seedIndex] = calculateBinIndex(data.hash,
+								seedIndex, hashShift, binSize) * CHUNKS_PER_BIN;
+
+						for (unsigned char chunkIndex = 0; chunkIndex < CHUNKS; chunkIndex++)
 						{
-							const Counter counter =
-								// Avoid additional port for init
-								(!countersInitialized? 0 : counters[chunkIndex][bitOffset])
-								+ bitvector[bitOffset];
-							counters[chunkIndex][bitOffset] = counter;
-							localResult[bitOffset] = counter >= threshold;
-							/*if (data.isLastElement)
-								printf("%d %d\n", counter, threshold);*/
+							Chunk bitvector = ~(Chunk)0;
+							Chunk localResult = 0;
+
+							// Unroll: Burst-coalesced over chunks per seed
+							#pragma unroll
+							for (unsigned char seedIndex = 0; seedIndex < HASH_COUNT; ++seedIndex)
+								bitvector &= //__burst_coalesced_cached_load(
+									/*&*/ibfData[static_cast<size_t>(binOffsets[seedIndex]) + chunkIndex];//,
+									//1048576); // 1 MiB = 8 megabit
+									//65536); // 65536 byte = 512 kilobit (default)
+
+							#pragma unroll
+							for (ushort bitOffset = 0; bitOffset < CHUNK_BITS; ++bitOffset)
+							{
+								const Counter counter =
+									// Avoid additional port for init
+									(!countersInitialized? 0 : counters[chunkIndex][bitOffset])
+									+ bitvector[bitOffset];
+
+								counters[chunkIndex][bitOffset] = counter;
+
+								localResult[bitOffset] = counter >= threshold;
+
+								/*if (data.isLastElement)
+									printf("%d %d\n", counter, threshold);*/
+							}
+
+							if (data.isLastElement)
+								result[static_cast<size_t>(queryIndex * CHUNKS + chunkIndex)] = localResult;
 						}
-						if (data.isLastElement)
-							result[static_cast<size_t>(queryIndex * CHUNKS + chunkIndex)] = localResult;
+
+						countersInitialized = 1;
 					}
-					countersInitialized = 1;
+					while(!data.isLastElement);
 				}
-				while(!data.isLastElement);
-			}
+			});
 		});
 	});
 }
+
+} // namespace min_ibf_fpga::backend_sycl
---
+++ test/ibf/ibf_sycl_test.cpp
@@ -0,0 +1,136 @@
+
+
+#include "fixtures/all.hpp"
+
+#include <sycl/sycl.hpp>
+#include <sycl/ext/intel/fpga_extensions.hpp>
+#include <sycl/ext/intel/ac_types/ac_int.hpp>
+
+#include <min_ibf_fpga/backend_sycl/kernel_ibf.hpp>
+#include <min_ibf_fpga/backend_sycl/exception_handler.hpp>
+#include <min_ibf_fpga/index/ibf_metadata.hpp>
+#include <min_ibf_fpga/index/ibf_data.hpp>
+
+#include <min_ibf_fpga/test/assert_equal.hpp>
+#include <min_ibf_fpga/test/assert_loaded_ibf.hpp>
+#include <min_ibf_fpga/test/load_ibf_index.hpp>
+
+void sycl_test(min_ibf_fpga::index::ibf_metadata const & ibf_meta, min_ibf_fpga::index::ibf_data const & ibf, ibf_test_fixture const & test)
+{
+#if defined(FPGA_EMULATOR)
+    sycl::ext::intel::fpga_emulator_selector device_selector;
+#else
+    sycl::ext::intel::fpga_selector device_selector;
+#endif
+    sycl::queue q(device_selector, fpga_tools::exception_handler);
+
+    std::vector<uint64_t> minimizer = test.minimizer();
+    std::vector<min_ibf_fpga::backend_sycl::MinimizerToIBFData> minimizerToIbf;
+    for (size_t i = 0; i < minimizer.size(); i++)
+    {
+        min_ibf_fpga::backend_sycl::MinimizerToIBFData data;
+        data.hash = minimizer[i];
+        data.isLastElement = false;
+        if(i + 1 == minimizer.size())
+        {
+            data.isLastElement = true;
+        }
+        minimizerToIbf.push_back(data);
+    }
+
+    std::vector<min_ibf_fpga::backend_sycl::Chunk> ibfData;
+    for (size_t i = 0; i < ibf.size(); i++)
+    {
+        ibfData.push_back(ibf.data()[i]);
+    }
+
+    auto compute_bin_containment_from_minimizer = [&](size_t const errors) -> std::bitset<64>
+    {
+        size_t const binSize = ibf_meta.bin_size();
+        size_t const hashShift = ibf_meta.hash_shift();
+        size_t const numberOfQueries = 1;
+        size_t const minimalNumberOfMinimizers = test.minimizer_range.minimal;
+        size_t const maximalNumberOfMinimizers = test.minimizer_range.maximal;
+        min_ibf_fpga::threshold::threshold_table ths = test.threshold_table_per_error[errors];
+            std::vector<min_ibf_fpga::backend_sycl::HostSizeType> thresholds;
+        for (size_t i = 0; i < ths.table.size(); i++)
+        {
+            thresholds.push_back(ths.table[i]);
+        }
+        std::vector<min_ibf_fpga::backend_sycl::Chunk> result(numberOfQueries, 0);
+
+        { // device buffer scope
+            sycl::buffer minimizerToIbf_buffer(minimizerToIbf);
+            sycl::buffer ibfData_buffer(ibfData);
+            sycl::buffer thresholds_buffer(thresholds);
+            sycl::buffer result_buffer(result);
+
+            min_ibf_fpga::backend_sycl::RunIBFKernel(q,
+                    minimizerToIbf_buffer,
+                    ibfData_buffer,
+                    binSize,
+                    hashShift,
+                    numberOfQueries,
+                    minimalNumberOfMinimizers,
+                    maximalNumberOfMinimizers,
+                    thresholds_buffer,
+                    result_buffer);
+        } // device buffer scope
+
+        // TODO: this needs adjustment if bin_count > 64
+        if (ibf_meta.bin_count() > 64) throw std::runtime_error{"NEEDS TO BE FIXED!!!!"};
+
+        std::bitset<64> bin_bit_membership{result[0]};
+        return bin_bit_membership;
+    };
+
+    // std::map<size_t, std::bitset<64>> minimizer_bit_membership_actual{};
+    // std::array<size_t, 64> counter_actual{};
+    std::vector<std::bitset<64>> bin_bit_membership_per_error{};
+    for (size_t error = 0; error < test.threshold_per_error.size(); ++error)
+    {
+        std::bitset<64> bin_bit_membership = compute_bin_containment_from_minimizer(error);
+
+        bin_bit_membership_per_error.push_back(bin_bit_membership);
+        std::cout << "bin_bit_membership: " << bin_bit_membership << std::endl;
+    }
+
+    // test.compare_minimizer_membership(minimizer_bit_membership_actual);
+    // test.compare_counter(counter_actual);
+    test.compare_bin_bit_membership(bin_bit_membership_per_error);
+}
+
+int main()
+{
+    // raptor_23_19.index
+    size_t const bin_count{64};
+    size_t const bin_size{1024};
+
+    auto [ibf_w23_k19_meta, ibf_w23_k19] = min_ibf_fpga::test::load_ibf_index("raptor_23_19.index");
+    auto [ibf_w19_k19_meta, ibf_w19_k19] = min_ibf_fpga::test::load_ibf_index("raptor_19_19.index");
+
+    min_ibf_fpga::test::assert_loaded_ibf(ibf_w23_k19_meta, min_ibf_fpga::test::ibf_meta_assertion_t {
+        .bin_count = bin_count,
+        .bin_size = bin_size
+    });
+
+    min_ibf_fpga::test::assert_loaded_ibf(ibf_w19_k19_meta, min_ibf_fpga::test::ibf_meta_assertion_t {
+        .bin_count = bin_count,
+        .bin_size = bin_size
+    });
+
+    sycl_test(ibf_w23_k19_meta, ibf_w23_k19, ibf_w23_k19_query0_test);
+    sycl_test(ibf_w23_k19_meta, ibf_w23_k19, ibf_w23_k19_query1_test);
+    sycl_test(ibf_w23_k19_meta, ibf_w23_k19, ibf_w23_k19_query2_test);
+    sycl_test(ibf_w23_k19_meta, ibf_w23_k19, ibf_w23_k19_query3_test);
+    sycl_test(ibf_w23_k19_meta, ibf_w23_k19, ibf_w23_k19_query4_test);
+
+    // not supported yet
+    //sycl_test(ibf_w19_k19_meta, ibf_w19_k19, ibf_w19_k19_query0_test);
+    //sycl_test(ibf_w19_k19_meta, ibf_w19_k19, ibf_w19_k19_query1_test);
+    //sycl_test(ibf_w19_k19_meta, ibf_w19_k19, ibf_w19_k19_query2_test);
+    //sycl_test(ibf_w19_k19_meta, ibf_w19_k19, ibf_w19_k19_query3_test);
+    //sycl_test(ibf_w19_k19_meta, ibf_w19_k19, ibf_w19_k19_query4_test);
+
+    return 0;
+}
