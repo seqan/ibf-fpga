9703949ff84bcb53d1d73296f163124a3727fc91
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2023-07-08 00:02:30 +0200

    [test] minimizer prepare to support multiple instances of different (w,k)

--- include/min_ibf_fpga/backend_sycl/kernel_minimizer_test.cpp
+++ include/min_ibf_fpga/backend_sycl/kernel_minimizer_test.cpp
@@ -6,29 +6,24 @@
 
 // Kernel includes
 #include "kernel.hpp"
-#include "kernel_minimizer.hpp"
 #include "sycl_kernel_minimizer.hpp"
 
 namespace min_ibf_fpga::backend_sycl
 {
 
-// Forward declaration of the kernel names. FPGA best practice to reduce compiler name mangling in the optimization reports.
-class MinimizerKernel;
-class PipeToHostKernel;
-
+template <typename SyclMinimizerKernel, typename pipe_to_host_kernel_t>
 void RunMinimizerKernel(sycl::queue& queue,
 	sycl::buffer<char, 1>& queries_buffer,
-	const _types::HostSizeType queriesOffset,
-	sycl::buffer<_types::HostSizeType, 1>& querySizes_buffer,
-	const _types::HostSizeType querySizesOffset,
-	const _types::HostSizeType numberOfQueries,
-	sycl::buffer<_types::MinimizerToIBFData, 1>& minimizerToIbf_buffer)
+	const typename SyclMinimizerKernel::type::HostSizeType queriesOffset,
+	sycl::buffer<typename SyclMinimizerKernel::type::HostSizeType, 1>& querySizes_buffer,
+	const typename SyclMinimizerKernel::type::HostSizeType querySizesOffset,
+	const typename SyclMinimizerKernel::type::HostSizeType numberOfQueries,
+	sycl::buffer<typename SyclMinimizerKernel::type::MinimizerToIBFData, 1>& minimizerToIbf_buffer)
 {
-	using constants = _constants;
-	using types = _types;
+	using sycl_minimizer_kernel_t = typename SyclMinimizerKernel::type;
+	using constants = typename sycl_minimizer_kernel_t::constants;
 
-	using sycl_minimizer_kernel_t = sycl_minimizer_kernel<constants, types>;
-	using MinimizerToIBFData = types::MinimizerToIBFData;
+	using MinimizerToIBFData = typename sycl_minimizer_kernel_t::MinimizerToIBFData;
 	using MinimizerToIBFPipes = typename sycl_minimizer_kernel_t::MinimizerToIBFPipes;
 
 	fpga_tools::UnrolledLoop<constants::number_of_kernels>([&](auto id)
@@ -38,15 +33,15 @@ void RunMinimizerKernel(sycl::queue& queue,
 		queue.submit([&](sycl::handler &handler)
 		{
 			sycl_minimizer_kernel_t minimizer_kernel{
-				.queries{queries_buffer, handler, sycl::read_only},
-				.queriesOffset{queriesOffset},
-				.querySizes{querySizes_buffer, handler, sycl::read_only},
-				.querySizesOffset{querySizesOffset},
-				.numberOfQueries{numberOfQueries}
+				/*.queries*/sycl::accessor{queries_buffer, handler, sycl::read_only},
+				/*.queriesOffset*/{queriesOffset},
+				/*.querySizes*/sycl::accessor{querySizes_buffer, handler, sycl::read_only},
+				/*.querySizesOffset*/{querySizesOffset},
+				/*.numberOfQueries*/{numberOfQueries}
 			};
-			handler.single_task<MinimizerKernel>([minimizer_kernel]() [[intel::kernel_args_restrict]]
+			handler.single_task<SyclMinimizerKernel>([minimizer_kernel]() [[intel::kernel_args_restrict]]
 			{
-				minimizer_kernel.execute<pipe_id>();
+				minimizer_kernel.template execute<pipe_id>();
 			});
 		});
 
@@ -54,13 +49,14 @@ void RunMinimizerKernel(sycl::queue& queue,
 		{
 			sycl::accessor minimizerToIbf(minimizerToIbf_buffer, handler, sycl::write_only);
 
-			handler.single_task<PipeToHostKernel>([=]() [[intel::kernel_args_restrict]]
+			handler.single_task<pipe_to_host_kernel_t>([=]() [[intel::kernel_args_restrict]]
 			{
 				size_t numberofMinimizersFound = 0;
 				MinimizerToIBFData data;
 				do
 				{
-					data = MinimizerToIBFPipes::PipeAt<id>::read();
+					using pipe_t = typename MinimizerToIBFPipes::template PipeAt<pipe_id>;
+					data = pipe_t::read();
 					minimizerToIbf[numberofMinimizersFound] = data;
 					numberofMinimizersFound++;
 				}
