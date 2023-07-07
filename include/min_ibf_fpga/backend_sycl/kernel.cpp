2d55834ccf672f11f4deb79717c655247d8bc180
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2023-07-07 23:06:54 +0200

    [sycl] minimizer: get rid of kernel_ibf.cpp

--- include/min_ibf_fpga/backend_sycl/kernel.cpp
+++ include/min_ibf_fpga/backend_sycl/kernel.cpp
@@ -59,7 +59,23 @@ void RunKernel(sycl::queue& queue,
 			});
 		});
 
-	#include "kernel_ibf.cpp"
+		queue.submit([&](sycl::handler &handler)
+		{
+			sycl_ibf_kernel_t ibf_kernel{
+				.ibfData{ibfData_buffer, handler, sycl::read_only},
+				.binSize{binSize},
+				.hashShift{hashShift},
+				.numberOfQueries{numberOfQueries},
+				.minimalNumberOfMinimizers{minimalNumberOfMinimizers},
+				.maximalNumberOfMinimizers{maximalNumberOfMinimizers},
+				.thresholds{thresholds_buffer, handler, sycl::read_only},
+				.result{result_buffer, handler, sycl::write_only},
+			};
+			handler.single_task<IbfKernel>([ibf_kernel]() [[intel::kernel_args_restrict]]
+			{
+				ibf_kernel.execute<pipe_id>();
+			});
+		});
 
 	});
 }
--- include/min_ibf_fpga/backend_sycl/kernel_ibf_test.cpp
+++ include/min_ibf_fpga/backend_sycl/kernel_ibf_test.cpp
@@ -51,7 +51,23 @@ void RunIBFKernel(sycl::queue& queue,
 			});
 		});
 
-		#include "kernel_ibf.cpp"
+		queue.submit([&](sycl::handler &handler)
+		{
+			sycl_ibf_kernel_t ibf_kernel{
+				.ibfData{ibfData_buffer, handler, sycl::read_only},
+				.binSize{binSize},
+				.hashShift{hashShift},
+				.numberOfQueries{numberOfQueries},
+				.minimalNumberOfMinimizers{minimalNumberOfMinimizers},
+				.maximalNumberOfMinimizers{maximalNumberOfMinimizers},
+				.thresholds{thresholds_buffer, handler, sycl::read_only},
+				.result{result_buffer, handler, sycl::write_only},
+			};
+			handler.single_task<IbfKernel>([ibf_kernel]() [[intel::kernel_args_restrict]]
+			{
+				ibf_kernel.execute<pipe_id>();
+			});
+		});
 
 	});
 }
