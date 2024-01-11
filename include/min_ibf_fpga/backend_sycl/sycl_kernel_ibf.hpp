3df7a98b5ee9195f33295ca04bdecaf3627721d1
Tobias Baumann <tobias.baumann@zib.de>
2024-01-11 10:45:18 +0100

    Merge kernel_*.hpp into sycl_kernel_*.hpp, keep kernel_*.hpp for tests to work

--- include/min_ibf_fpga/backend_sycl/sycl_kernel_ibf.hpp
+++ include/min_ibf_fpga/backend_sycl/sycl_kernel_ibf.hpp
@@ -3,8 +3,6 @@
 #include <sycl/sycl.hpp>
 #include <sycl/ext/intel/fpga_extensions.hpp>
 
-#include <min_ibf_fpga/backend_sycl/kernel_ibf.hpp>
-
 namespace min_ibf_fpga::backend_sycl
 {
 
@@ -19,8 +17,7 @@ struct sycl_ibf_kernel
 	using Counter = typename types::Counter;
 	using Chunk = typename types::Chunk;
 	using Hash = typename types::Hash;
-
-	using ibf_kernel_t = ibf_kernel<constants, types>;
+	using HostHash = typename types::HostHash;
 
 	using MinimizerToIBFData = typename types::MinimizerToIBFData;
 	using MinimizerToIBFPipes = _MinimizerToIBFPipes;
@@ -34,12 +31,44 @@ struct sycl_ibf_kernel
 	const HostSizeType* thresholds{};
 	Chunk* result{};
 
+	static inline HostSizeType mapTo(const HostHash hash, const HostSizeType binSize)
+	{
+		using DoubleHostSizeType = typename types::DoubleHostSizeType;
+
+		return ((DoubleHostSizeType)hash * (DoubleHostSizeType)binSize) >> constants::host_size_type_bits;
+	}
+
+	static inline HostSizeType calculateBinIndex(HostHash hash, const unsigned char seedIndex, const HostSizeType hashShift, const HostSizeType binSize)
+	{
+		hash *= constants::seeds[seedIndex];
+		hash ^= hash >> hashShift;
+		hash *= 11400714819323198485u;
+
+		return mapTo(hash, binSize);
+	}
+
+	// inline Counter getThreshold(const HostSizeType numberOfHashes,
+	// 	const HostSizeType minimalNumberOfMinimizers,
+	// 	const HostSizeType maximalNumberOfMinimizers,
+	// 	HostSizeType* thresholds) //__global HostSizeType* restrict thresholds
+	// {
+	// 	const HostSizeType maximalIndex = maximalNumberOfMinimizers - minimalNumberOfMinimizers;
+
+	// 	HostSizeType index = numberOfHashes < minimalNumberOfMinimizers?
+	// 		0 : numberOfHashes - minimalNumberOfMinimizers;
+	// 	index = index < maximalIndex? index : maximalIndex;
+
+	// 	return (thresholds[index] + 2).to_uint();
+	// }
+
 	template <size_t pipe_id>
 	void execute() const
 	{
 		sycl::device_ptr<const HostSizeType> thresholds_ptr(thresholds);
 		sycl::device_ptr<const Chunk> ibfData_ptr(ibfData);
 		sycl::device_ptr<Chunk> result_ptr(result);
+
+		using pipe_t = typename MinimizerToIBFPipes::template PipeAt<pipe_id>;
 
 		// TODO: adjust size
 		[[intel::fpga_memory]] size_t idx_reg[100000];
@@ -48,37 +77,114 @@ struct sycl_ibf_kernel
 		[[intel::initiation_interval(1)]]
 		for (QueryIndex queryIndex = 0; queryIndex < (QueryIndex)numberOfQueries; queryIndex++)
 		{
-			ibf_kernel_t::compute_ibf(
-				ibfData_ptr,
-				binSize,
-				hashShift,
-			[&](const QueryIndex localNumberOfHashes){
-				//threshold = getThreshold(localNumberOfHashes, minimalNumberOfMinimizers, maximalNumberOfMinimizers, thresholds);
-				// manual inline of getThreshold() because of thresholds accessor
-				const HostSizeType maximalIndex = maximalNumberOfMinimizers - minimalNumberOfMinimizers;
+			#define UNSAFELEN 9 // LD singlepump pump (7) + Arithmetic (1) + Store (1)
+			#define II (UNSAFELEN - CHUNKS)
 
-				HostSizeType index = localNumberOfHashes < minimalNumberOfMinimizers? 0 : localNumberOfHashes - minimalNumberOfMinimizers;
-				index = index < maximalIndex? index : maximalIndex;
+			#if II > 1
+				Counter counters[constants::chunks][constants::chunk_bits]
+		#if MIN_IBF_FPGA_INIT_EVERYTHING
+					= {}
+		#endif // MIN_IBF_FPGA_INIT_EVERYTHING
+				; // __attribute__((register))
+			#else
+				Counter __attribute__((
+						memory,
+						numbanks(1),
+						bankwidth(constants::chunk_bits * sizeof(Counter)),
+						singlepump,//singlepump,
+						max_replicates(1)))
+					counters[constants::chunks][constants::chunk_bits];
+			#endif
 
-				Counter threshold = (thresholds_ptr[static_cast<size_t>(index)] + 2).to_uint();
-				return threshold;
-			}, [&]() -> MinimizerToIBFData {
-				using pipe_t = typename MinimizerToIBFPipes::template PipeAt<pipe_id>;
-				return pipe_t::read();
-			}, [&](size_t const chunkIndex, Chunk const & localResult) {
-				size_t result_idx = static_cast<size_t>(queryIndex) * constants::chunks + chunkIndex;
+			bool countersInitialized = 0;
 
-				idx_reg[queryIndex] = result_idx;
-				result_reg[queryIndex] = localResult;
+			QueryIndex numberOfHashes = 1;
 
-			}, [&](unsigned char const chunk_idx, Hash const & minimizer, Chunk const & minimizer_membership) {
-				// no-op this is for debugging
-			}, [&](unsigned char const chunk_idx, Counter const * counters, size_t const counters_size) {
-				// no-op this is for debugging
-			});
+			MinimizerToIBFData data;
+
+			#if II <= 1
+				#pragma ivdep array(counters)
+			#endif
+			do
+			{
+				data = pipe_t::read();
+
+				const QueryIndex localNumberOfHashes = numberOfHashes++;
+
+				Counter threshold;
+
+		#if MIN_IBF_FPGA_INIT_EVERYTHING
+				threshold = 0; // Without this the code below has UB on CPU
+		#endif // MIN_IBF_FPGA_INIT_EVERYTHING
+
+				if (data.isLastElement) {
+					//threshold = getThreshold(localNumberOfHashes, minimalNumberOfMinimizers, maximalNumberOfMinimizers, thresholds);
+					// manual inline of getThreshold() because of thresholds accessor
+					const HostSizeType maximalIndex = maximalNumberOfMinimizers - minimalNumberOfMinimizers;
+
+					HostSizeType index = localNumberOfHashes < minimalNumberOfMinimizers? 0 : localNumberOfHashes - minimalNumberOfMinimizers;
+					index = index < maximalIndex? index : maximalIndex;
+
+					threshold = (thresholds_ptr[static_cast<size_t>(index)] + 2).to_uint();
+				}
+
+				HostSizeType binOffsets[constants::hash_count]
+		#if MIN_IBF_FPGA_INIT_EVERYTHING
+					= {}
+		#endif // MIN_IBF_FPGA_INIT_EVERYTHING
+				;
+
+				#pragma unroll
+				for (unsigned char seedIndex = 0; seedIndex < constants::hash_count; ++seedIndex)
+					binOffsets[seedIndex] = calculateBinIndex(data.hash,
+						seedIndex, hashShift, binSize) * constants::chunks_per_bin;
+
+				for (unsigned char chunkIndex = 0; chunkIndex < constants::chunks; chunkIndex++)
+				{
+					Chunk bitvector = ~(Chunk)0;
+					Chunk localResult{0};
+
+					// Unroll: Burst-coalesced over chunks per seed
+					#pragma unroll
+					for (unsigned char seedIndex = 0; seedIndex < constants::hash_count; ++seedIndex)
+						bitvector &= //__burst_coalesced_cached_load(
+							/*&*/ibfData[static_cast<size_t>(binOffsets[seedIndex]) + chunkIndex];//,
+							//1048576); // 1 MiB = 8 megabit
+							//65536); // 65536 byte = 512 kilobit (default)
+
+					#pragma unroll
+					for (ushort bitOffset = 0; bitOffset < constants::chunk_bits; ++bitOffset)
+					{
+						const Counter counter =
+							// Avoid additional port for init
+							(!countersInitialized? 0 : counters[chunkIndex][bitOffset])
+							+ bitvector[bitOffset];
+
+						counters[chunkIndex][bitOffset] = counter;
+
+						// Note: threshold might be UNDEFINED if
+						localResult[bitOffset] = counter >= threshold;
+
+						/*if (data.isLastElement)
+							printf("%d %d\n", counter, threshold);*/
+					}
+
+					if (data.isLastElement)
+					{
+						size_t result_idx = static_cast<size_t>(queryIndex) * constants::chunks + chunkIndex;
+
+						idx_reg[queryIndex] = result_idx;
+						result_reg[queryIndex] = localResult;
+					}
+				}
+
+				countersInitialized = 1;
+			}
+			while(!data.isLastElement);
 		}
 
-		for (QueryIndex queryIndex = 0; queryIndex < (QueryIndex)numberOfQueries; queryIndex++) {
+		for (QueryIndex queryIndex = 0; queryIndex < (QueryIndex)numberOfQueries; queryIndex++)
+		{
 			result_ptr[idx_reg[queryIndex]] = result_reg[queryIndex];
 		}
 	}
--- include/min_ibf_fpga/backend_sycl/sycl_kernel_minimizer.hpp
+++ include/min_ibf_fpga/backend_sycl/sycl_kernel_minimizer.hpp
@@ -3,8 +3,6 @@
 #include <sycl/sycl.hpp>
 #include <sycl/ext/intel/fpga_extensions.hpp>
 
-#include <min_ibf_fpga/backend_sycl/kernel_minimizer.hpp>
-
 #include <min_ibf_fpga/backend_sycl/pipe_utils.hpp>
 
 namespace min_ibf_fpga::backend_sycl
@@ -18,7 +16,9 @@ struct sycl_minimizer_kernel
 
 	using HostSizeType = typename types::HostSizeType;
 	using QueryIndex = typename types::QueryIndex;
-	using minimizer_kernel_t = minimizer_kernel<constants, types>;
+	using Element = typename types::Element;
+	using Hash = typename types::Hash;
+	using Minimizer = typename types::Minimizer;
 
 	using PrefetchingLSU = sycl::ext::intel::lsu<sycl::ext::intel::prefetch<true>, sycl::ext::intel::statically_coalesce<false>>;
 
@@ -31,11 +31,77 @@ struct sycl_minimizer_kernel
 	HostSizeType querySizesOffset{};
 	HostSizeType numberOfQueries{};
 
+	static Element inline translateCharacterToElement(const char character)
+	{
+		return character == 'A'? 0
+			: character == 'C'? 1
+			: character == 'G'? 2
+			: 3;
+	}
+
+	static Hash inline extractHash(const char* buffer) //, sycl::stream out) // DEBUG
+	{
+		Hash kmer = 0;
+		Hash kmerComplement = 0;
+
+		#pragma unroll
+		for (unsigned char elementIndex = 0; elementIndex < constants::min_ibf_k; ++elementIndex)
+		{
+			const Element value = translateCharacterToElement(buffer[elementIndex]);
+
+			//out << elementIndex << ": " << static_cast<unsigned>(kmer) << " - " << static_cast<unsigned>(kmerComplement) << sycl::endl;
+			//	out << elementIndex << ": ";
+			//for (unsigned i = 0; i < 2*MIN_IBF_K; i++)
+			//	out << (unsigned)kmer[i];
+			//out << " - ";
+			//for (unsigned i = 0; i < 2*MIN_IBF_K; i++)
+			//	out << (unsigned)kmerComplement[i];
+			//out << sycl::endl;
+
+			//out << "char " << static_cast<unsigned>(buffer[elementIndex]) << " value " << static_cast<unsigned>(value) << " ~value " << static_cast<unsigned>((Element)~value) << sycl::endl;
+
+			kmer           |= (Hash)(value) << (2 * (constants::min_ibf_k - 1) - elementIndex * 2);
+			kmerComplement |= (Hash)((Element)~value) << elementIndex * 2;
+		}
+
+		//out << "Extracted Hash " << static_cast<unsigned>(kmer) << " Complement " << static_cast<unsigned>(kmerComplement) << sycl::endl;
+		//out << "XH ";
+		//for (unsigned i = 0; i < 2*MIN_IBF_K; i++)
+		//	out << (unsigned)kmer[i];
+		//out << " C ";
+		//for (unsigned i = 0; i < 2*MIN_IBF_K; i++)
+		//	out << (unsigned)kmerComplement[i];
+		//out << sycl::endl;
+
+		kmer ^= (Hash)constants::minimizer_seed_adjusted;
+		kmerComplement ^= (Hash)constants::minimizer_seed_adjusted;
+
+		return kmer < kmerComplement? kmer : kmerComplement;
+	}
+
+	static Minimizer inline findMinimizer(const Hash* hashBuffer)
+	{
+		Minimizer minimizer = {~(Hash)0, 0};
+
+		#pragma unroll
+		for (unsigned char kmerIndex = 0; kmerIndex < constants::number_of_kmers_per_window; ++kmerIndex)
+		{
+			const Minimizer current = {hashBuffer[kmerIndex], kmerIndex};
+
+			if (current.hash < minimizer.hash)
+				minimizer = current;
+		}
+
+		return minimizer;
+	}
+
 	template <size_t pipe_id>
 	void execute() const
 	{
 		sycl::device_ptr<const char> queries_ptr(queries);
 		sycl::device_ptr<const HostSizeType> querySizes_ptr(querySizes);
+
+		using pipe_t = typename MinimizerToIBFPipes::template PipeAt<pipe_id>;
 
 		QueryIndex queryOffset = queriesOffset;
 
@@ -46,13 +112,71 @@ struct sycl_minimizer_kernel
 			const QueryIndex localQueryOffset = queryOffset;
 			queryOffset += querySize;
 
-			minimizer_kernel_t::compute_minimizer(querySize, [&](QueryIndex const iteration){
-				return PrefetchingLSU::load(queries_ptr + static_cast<size_t>(localQueryOffset + iteration));
-			}, [&](auto && data)
+				//out << constants::initialization_iterations << " + " << (unsigned)querySize << " - " << WINDOW_SIZE << " + 1" << sycl::endl; // DEBUG
+
+			const QueryIndex iterations =
+				constants::initialization_iterations // Fill query and hash buffer initially
+				+ querySize - constants::window_size + 1;
+
+			char queryBuffer[constants::min_ibf_k] = {0};
+			Hash hashBuffer[constants::number_of_kmers_per_window] = {0};
+
+			// Set initial element's position to 0, so the first real element will never be skipped
+			Minimizer lastMinimizer = {0, 0};
+
+			for (QueryIndex iteration = 0; iteration <= iterations; iteration++)
 			{
-				using pipe_t = typename MinimizerToIBFPipes::template PipeAt<pipe_id>;
-				pipe_t::write(std::forward<decltype(data)>(data));
-			});
+				// DEBUG
+				//out << static_cast<unsigned>(iteration) << ": " << queryBuffer << " - ";
+				//for (size_t i = 0; i < constants::min_ibf_k; i++)
+				//	out << queryBuffer[i];
+				//out << " - ";
+				//for (size_t i = 0; i < constants::number_of_kmers_per_window; i++)
+				//	out << static_cast<unsigned>(hashBuffer[i]) << ",";
+				//out << sycl::endl;
+
+				// Shift register: Query buffer
+				#pragma unroll
+				for (unsigned char i = 0; i < constants::min_ibf_k - 1; ++i)
+					queryBuffer[i] = queryBuffer[i + 1];
+
+				// Query as long as elements are left, then only do calculations (end phase)
+				if (iteration < querySize)
+					queryBuffer[constants::min_ibf_k - 1] = PrefetchingLSU::load(queries_ptr + static_cast<size_t>(localQueryOffset + iteration));
+
+				// Shift register: hash buffer
+				#pragma unroll
+				for (ushort i = 0; i < constants::number_of_kmers_per_window - 1; ++i)
+					hashBuffer[i] = hashBuffer[i + 1];
+
+				hashBuffer[constants::number_of_kmers_per_window - 1] = extractHash(queryBuffer); // , out); // DEBUG
+
+				const Minimizer minimizer = findMinimizer(hashBuffer);
+				const Minimizer localLastMinimizer = lastMinimizer;
+
+				if (iteration >= constants::initialization_iterations)
+					// Update the lastMinimizer every time, because it is either new or the same anyways
+					lastMinimizer = minimizer;
+
+				const bool skipMinimizer = localLastMinimizer.position != 0
+					&& localLastMinimizer.hash == minimizer.hash;
+				const bool lastElement = iteration > iterations - 1;
+
+				if (// Skip the first element (> instead of >=), as lastMinimizer is not initialized yet
+					iteration > constants::initialization_iterations
+					// Send the lastMinimizer when a new one is found or the last element is reached
+					&& (!skipMinimizer || lastElement))
+				{
+					MinimizerToIBFData data;
+					data.isLastElement = lastElement;
+					data.hash = localLastMinimizer.hash;
+
+					// DEBUG
+					//out << "Found minimizer: " << static_cast<unsigned>(data.hash) << " " << data.isLastElement << sycl::endl; // DEBUG
+
+					pipe_t::write(data);
+				}
+			}
 		}
 	}
 };
